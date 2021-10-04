a8b7060eabe9e9c059d36e8a71bbb40837cb1315
KamoMaimela <2103503@students.wits.ac.za>
2021-10-04 13:16:24 +0200

    Bubble destroyer to avoid particle system getting to clustered on the hierarchy

---
+++ Semester Project  - Viva Aquarium/Assets/Scripts/DestroyBubble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBubble : MonoBehaviour
+{
+
+    public float LifeTime = 5f;
+    
+    void Start()
+    {
+        Invoke("Destroy", LifeTime);  //Destroy bubble after specific amount of seconds has passed
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+   
+}
