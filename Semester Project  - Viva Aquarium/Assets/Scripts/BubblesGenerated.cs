c052db49999d56d3336cbee3abbb19ce8e7fb227
KamoMaimela <2103503@students.wits.ac.za>
2021-10-12 17:36:29 +0200

    Fish Info in shop

--- Semester Project  - Viva Aquarium/Assets/Scripts/BubblesGenerated.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/BubblesGenerated.cs
@@ -39,7 +39,7 @@ public class BubblesGenerated : MonoBehaviour
         seconds += Time.deltaTime;
 
         
-        if (seconds >= 1)
+        if (seconds >= 3)
         {
             bubbles += bubbleproduction;      //+1 each time a bubble spawns
             
