12f30ee5e108a129cdd75fed9abbc0be1b62c195
KamoMaimela <2103503@students.wits.ac.za>
2021-10-12 12:01:17 +0200

    Added prices to 2nd and 3rd fish in shop

--- Semester Project  - Viva Aquarium/Assets/Scripts/FishShop.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/FishShop.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class FishShop : MonoBehaviour
 {
     public GameObject FishPrefab;
-
     public Transform FishSpawnPoint;
 
-    public void FishSelect()
+    public GameObject FishPrefab02;
+    public Transform FishSpawnPoint02;
+
+    public GameObject FishPrefab03;
+    public Transform FishSpawnPoint03;
+
+    public void FishSelect() //This function is for the first fish
     {
         if (BubbleManager.Count >= 10)  //Players can only buy Fish1 once they have this amount of bubbles
         {
@@ -18,5 +23,31 @@ public class FishShop : MonoBehaviour
             GameObject.Find("Tank01 Info").GetComponent<InfoTank01>().FishInTank += 1;
         }
         
+    }
+
+
+    public void FishSelect02() //Function for the second fish
+    {
+        if (BubbleManager.Count >= 60)  
+        {
+            Instantiate(FishPrefab02, FishSpawnPoint02.position, FishSpawnPoint02.rotation);
+            BubblesGenerated.bubbles -= 60;
+
+            GameObject.Find("Tank01 Info").GetComponent<InfoTank01>().FishInTank += 1;
+        }
+
+    }
+
+
+    public void FishSelect03() //Function for the third fish
+    {
+        if (BubbleManager.Count >= 150)
+        {
+            Instantiate(FishPrefab03, FishSpawnPoint03.position, FishSpawnPoint03.rotation);
+            BubblesGenerated.bubbles -= 150;
+
+            GameObject.Find("Tank01 Info").GetComponent<InfoTank01>().FishInTank += 1;
+        }
+
     }
 }
