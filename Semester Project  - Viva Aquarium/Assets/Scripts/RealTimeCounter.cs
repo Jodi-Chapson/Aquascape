af111389ad1c6d062c53a92d6486818c57b850f8
KamoMaimela <2103503@students.wits.ac.za>
2021-10-25 05:12:38 +0200

    Fish information added in Shop

--- Semester Project  - Viva Aquarium/Assets/Scripts/RealTimeCounter.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/RealTimeCounter.cs
@@ -10,7 +10,7 @@ public class RealTimeCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = 43200;
+        timer = 43200; //This is 12 hours
 
         timer -= TimeMaster.instance.CheckDate();
     }
