5eba070c9e9eecd17b4ee9163e8d6e8be0cb0e00
Jodi Chapson <2152594@students.wits.ac.za>
2021-10-27 13:44:55 +0200

    Removed the feeding feature temporarily

--- Semester Project  - Viva Aquarium/Assets/Scripts/InfoTank01.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/InfoTank01.cs
@@ -14,9 +14,9 @@ public class InfoTank01 : MonoBehaviour
     public float TankProductionModifer; //a percentage?
 
     public float UpgradeTankPrice = 20f; //Initial upgrade price for the tank
-    
 
-    public float FishInTank = 1f;       
+
+    public float FishInTank = 1f;
     public float FishAllowed = 8f;                  //Both these lines represent the capacity of the tank :)
 
     public GameObject FishFoodPrefab;
@@ -40,36 +40,37 @@ public class InfoTank01 : MonoBehaviour
     {
         if (BubbleManager.Count >= UpgradeTankPrice)  //Players can only upgrade fish tank once they only have this amount
         {
-            BubblesGenerated.bubbles -= (int) UpgradeTankPrice;
+            BubblesGenerated.bubbles -= (int)UpgradeTankPrice;
 
             FishAllowed += 2;
             CapacityText.text = "Capacity : " + FishInTank + "/" + FishAllowed;
 
             TankLevel += 1;
-            TankProductionModifer ++;
+            TankProductionModifer++;
             TankLevelText.text = "Tank Level : " + TankLevel;
 
 
             UpgradeTankPrice = (int)(UpgradeTankPrice * 1.5f);
             UpgradePrice.text = "Upgrade Cost: " + UpgradeTankPrice;
 
-           
+
         }
     }
 
-    void OnMouseOver()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Feed Fish
-            GameObject.Find("Fish").GetComponent<RealTimeCounter>().Hungry = false;
-            GameObject.Find("Fish").GetComponent<RealTimeCounter>().timer = 12;        //Reseting the Timer;
+    //    void OnMouseOver()
+    //    {
+    //        if (Input.GetMouseButtonDown(0))
+    //        {
+    //            //Feed Fish
+    //            GameObject.Find("Fish").GetComponent<RealTimeCounter>().Hungry = false;
+    //            GameObject.Find("Fish").GetComponent<RealTimeCounter>().timer = 12;        //Reseting the Timer;
 
 
-           // Instantiate(FishFoodPrefab, FoodSpawnPoint.position, FoodSpawnPoint.rotation);
+    //           // Instantiate(FishFoodPrefab, FoodSpawnPoint.position, FoodSpawnPoint.rotation);
 
-            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Instantiate(FishFoodPrefab, new Vector3(cursorPos.x, cursorPos.y, 0), Quaternion.identity);
-        }
-    }
+    //            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //            Instantiate(FishFoodPrefab, new Vector3(cursorPos.x, cursorPos.y, 0), Quaternion.identity);
+    //        }
+    //    }
+    //}
 }
