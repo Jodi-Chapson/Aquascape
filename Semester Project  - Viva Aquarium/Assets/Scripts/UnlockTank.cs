b1e6326ed96575c6952ab569c53772a858c93706
KamoMaimela <2103503@students.wits.ac.za>
2021-11-11 15:47:19 +0200

    Will finish these sets of code once electricity comes back at 6:30

--- Semester Project  - Viva Aquarium/Assets/Scripts/UnlockTank.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/UnlockTank.cs
@@ -9,28 +9,45 @@ public class UnlockTank : MonoBehaviour
     public Text PriceText;
     public GameObject Panel;
 
-    
-   public void Tank02()
+    public static int TankNumber;
+
+    public GameObject Tank2;
+    public GameObject Tank2Background;
+
+    public GameObject Tank3;
+    public GameObject Tank3Background;
+
+    public GameObject Tank4;
+    public GameObject Tank4Background;
+
+
+    public void Tank02()
     {
         Price = 1000;
         PriceText.text = "" + Price;
+
+        TankNumber = 2;
     }
 
     public void Tank03()
     {
         Price = 3000;
         PriceText.text = "" + Price;
+
+        TankNumber = 3;
     }
 
     public void Tank04()
     {
         Price = 9000;
         PriceText.text = "" + Price;
+
+        TankNumber = 4;
     }
 
     public void Yes() 
     {
-        if(BubblesGenerated.bubbles < Price)
+        if(BubblesGenerated.bubbles < Price)        //Check if player has enough funds to purchase a tank
         {
             Panel.SetActive(true);
         }
@@ -39,6 +56,22 @@ public class UnlockTank : MonoBehaviour
             Panel.SetActive(false);
 
             BubblesGenerated.bubbles -= Price;
+        }
+
+
+        if(TankNumber == 2)                              //Here we deactive the lock symbols and everything else on the tanks
+        {
+            Tank2.SetActive(false);
+        }
+
+        if (TankNumber == 3)
+        {
+            Tank3.SetActive(false);
+        }
+
+        if (TankNumber == 4)
+        {
+            Tank4.SetActive(false);
         }
     }
 }
