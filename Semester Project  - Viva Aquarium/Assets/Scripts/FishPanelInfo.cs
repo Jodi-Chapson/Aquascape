b924480ab4ab9b111471aa223f9785db4cc28433
Jodi Chapson <2152594@students.wits.ac.za>
2021-11-16 01:01:08 +0200

    Panels now have species name and picture :D

--- Semester Project  - Viva Aquarium/Assets/Scripts/FishPanelInfo.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/FishPanelInfo.cs
@@ -6,30 +6,57 @@ using UnityEngine.UI;
 public class FishPanelInfo : MonoBehaviour
 {
     public GameObject targetfish;
+    public GameManager manager;
     public GameObject onestar, twostar, threestar, fourstar, fivestar;
-    public Image fishsprite;
-    public Text UpgradePrice;
+    public GameObject fishsprite;
+    public Text UpgradePrice, FishSpecies;
     public GameObject Upgradebutton;
     public int UpgradeFishPrice;
     public int currentlevel;
 
 
+    public void Start()
+    {
+        manager = GameObject.Find("Bubble_Manager").GetComponent<GameManager>();
+        UpgradePrice.text = "Price: " + UpgradeFishPrice;
+    }
 
     public void LoadInfo(GameObject fish)
     {
-
+        manager = GameObject.Find("Bubble_Manager").GetComponent<GameManager>();
         targetfish = fish;
         currentlevel = fish.GetComponent<BubblesGenerated>().level;
+        FishSpecies.text = fish.GetComponent<Fish>().Species.ToString();
+
+
+        if (fish.GetComponent<Fish>().Species == "Gold Fish")
+        {
+            fishsprite.GetComponent<Image>().sprite = manager.panelsprites[0];
+            Debug.Log("nani1");
+        }
+        else if (fish.GetComponent<Fish>().Species == "Red Tailed Shark")
+        {
+            fishsprite.GetComponent<Image>().sprite = manager.panelsprites[1];
+            Debug.Log("nani2");
+        }
+        else if (fish.GetComponent<Fish>().Species == "Neon Tetra")
+        {
+            fishsprite.GetComponent<Image>().sprite = manager.panelsprites[2];
+            Debug.Log("nani3");
+        }
+        else
+        {
+            fishsprite.GetComponent<Image>().sprite = manager.panelsprites[3];
+            Debug.Log("nani4");
+        }
         
         
         UpdateStarSprites();
 
     }
 
-    public void Start()
-    {
-        UpgradePrice.text = "Price: " + UpgradeFishPrice;
-    }
+    
+    
 
     public void Upgrade()
     {
--- Semester Project  - Viva Aquarium/Assets/Scripts/GameManager.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public InfoTankManager[] tanks = new InfoTankManager[4]; //reference to all the relevant tanks, for assigning home tanks
     public GameObject[] fishtypes = new GameObject[4]; //reference to all the relevant fish prefabs
+    public Sprite[] panelsprites = new Sprite[4]; // reference for all panel sprites
     public int CurrentTankID;
 
 }
