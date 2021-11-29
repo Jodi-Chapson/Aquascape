4b1d1306eeeb533dd9a62228fade5939b3adbf86
Skaduvee <2093671@students.wits.ac.za>
2021-11-29 20:04:09 +0200

    Update RedTailedShark.cs
    Fixed some errors with the logic

--- Semester Project  - Viva Aquarium/Assets/Scripts/Preferences/RedTailedShark.cs
+++ Semester Project  - Viva Aquarium/Assets/Scripts/Preferences/RedTailedShark.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class RedTailedShark : MonoBehaviour
 {
     public GameObject RedTailedSharkPrefab;
-    public int Count = 0;
-    
-    private void Update()
+    [SerializeField] public static int Count = 0;
+    [SerializeField] public static bool isRedTailedFish;
+
+    private void Start()
     {
+        isRedTailedFish = false;
         CheckTankForOtherRedTailedSharks();
     }
 
     private void CheckTankForOtherRedTailedSharks()
     {
+        Debug.Log(GameManager.fish.Count);
+
         for (int i = 0; i < GameManager.fish.Count; i++)
         {
             if (GameManager.fish[i] == RedTailedSharkPrefab)
             {
-                if (Count <= 1)
+                if (Count == 0)
                 {
                     Count++;
                     Debug.Log("one red tailed shark is in there");
                 }
-                else if (Count > 1)
+                else
                 {
                     Count++;
+                    isRedTailedFish = true;
                     //code to decrease happiness.
                     Debug.Log("Multiple red tailed sharks");
                 }
