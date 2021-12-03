006269309eb0d541eb052f56acb7b5b180d241a2
Jodi Chapson <2152594@students.wits.ac.za>
2021-12-03 21:50:45 +0200

    Made Audio start from menu to main scene

---
+++ Semester Project  - Viva Aquarium/Assets/Scripts/Audio.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Audio : MonoBehaviour
+{
+    //this script is so that the audio does not get destroyed and can play throughout the game.
+    // however, there was an issue as multiple audio gameobjects could be created and played at once - if you return to the menu
+    // a solution to this issue was provided by https://www.youtube.com/watch?v=PHa5SNe1Mmk&t
+
+    public static Audio song;
+    
+
+
+
+
+   
+
+    private void Awake()
+    {
+
+
+
+        this.GetComponent<AudioSource>().volume = 0.1f;
+
+        this.GetComponent<AudioSource>().Play();
+
+        if (song != null)
+        {
+            //if there is indeed a version of song1 in the scene
+            Destroy(gameObject);
+
+
+
+        }
+        else
+        {
+            //there is no other instance
+            song = this;
+            DontDestroyOnLoad(transform.gameObject);
+
+
+        }
+
+    }
+
+   
+}
